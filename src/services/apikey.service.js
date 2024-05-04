a7b2fe2c13d5db7673aeb1ae88900bd471b23768
ngoctruongbui <ngoctruongbuii2003@gmail.com>
2024-05-04 18:36:59 +0700

    custom dynamic middleware for Apikey and Permission

---
+++ src/services/apikey.service.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const apiKeyModel = require('../models/apikey.model')
+const crypto = require('crypto')
+
+const findById = async (key) => {
+    // const newApiKey = await apiKeyModel.create({ key: crypto.randomBytes(64).toString('hex'), permissions: ['0000'] })
+    // console.log(`New apiKey::`, newApiKey);
+
+    const objKey = await apiKeyModel.findOne({ key, status: true }).lean()
+
+    return objKey
+}
+
+module.exports = {
+    findById
+}
